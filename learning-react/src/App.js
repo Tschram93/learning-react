1952447a965c90bc1c897e645090a4ecba4bff87
TS93 <tschram93@yahoo.com>
2021-12-08 16:41:24 -0500

    basic todo app completed; wiping files to start another project

--- learning-react/src/App.js
+++ learning-react/src/App.js
@@ -1,14 +1,5 @@
-import Todo from './components/Todo';
-
 function App() {
-	return (
-		<div>
-			<h1>My To-Do's</h1>
-			<Todo text="Learn React" />
-			<Todo text="Master React" />
-			<Todo text="Explore React" />
-		</div>
-	);
+	return;
 }
 
 export default App;
