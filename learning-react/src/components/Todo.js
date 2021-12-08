1ab1f595a353184f68c943bcdadde0af07d51c0d
TS93 <tschram93@yahoo.com>
2021-12-08 16:23:11 -0500

    clicking delete button onClick the openmodal function; clicking background onCancel closeModal function

--- learning-react/src/components/Todo.js
+++ learning-react/src/components/Todo.js
@@ -1,15 +1,28 @@
+import Backdrop from './Backdrop';
+import Modal from './Modal';
+import { useState } from 'react';
+
 function Todo(props) {
-    function deleteHandler() {
-        console.log("Clicked!");
-        console.log(props.text);
-    }
+	const [modalIsOpen, setModalIsOpen] = useState(false);
+
+	function deleteHandler() {
+		setModalIsOpen(true);
+	}
+
+	function closeModalHandler(){
+		setModalIsOpen(false);
+	}
 
 	return (
 		<div className="card">
 			<h2>{props.text}</h2>
 			<div className="actions">
-				<button className="btn" onClick={deleteHandler}>Delete</button>
+				<button className="btn" onClick={deleteHandler}>
+					Delete
+				</button>
 			</div>
+			{modalIsOpen && <Modal />}
+			{modalIsOpen && <Backdrop onCancel={closeModalHandler} />}
 		</div>
 	);
 }
